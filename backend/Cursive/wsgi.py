1bc50f2fd97d399d13875129e59a9755c4ca10e4
sky81219 <limhaneul12@gmail.com>
2022-08-01 01:58:03 +0900

    환경설정

---
+++ backend/Cursive/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Cursive project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Cursive.settings')
+
+application = get_wsgi_application()
