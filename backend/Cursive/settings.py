122480bb4fe2fd878e3afca61bd2c11a841cfe0f
sky81219 <limhaneul12@gmail.com>
2022-08-06 20:05:39 +0900

    환경설정 JWT

--- backend/Cursive/settings.py
+++ backend/Cursive/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'dashboard.apps.DashboardConfig',
     'rest_framework',
-    'rest_framework_simplejwt'
+    'rest_framework_simplejwt',
 ]
 
 REST_FRAMEWORK = {
--- backend/dashboard/models.py
+++ backend/dashboard/models.py
@@ -2,3 +2,18 @@ from django.contrib.auth.models import PermissionsMixin, BaseUserManager
 from django.contrib.auth.models import AbstractUser, AbstractBaseUser
 from django.db import models
 
+
+class AdminUserCustomManager(BaseUserManager):
+    def create_user(self, email: str, name: str, password: str=None) -> None:
+        if not email:
+            raise ValueError("Users must have an email address")
+        
+        user = self.model(
+            email=self.normalize_email(email),
+            name=name,
+        )
+        
+        user.set_password(password)
+        user.save(using=self._db)
+        
+        return user
